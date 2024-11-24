f13143b2a882483d230a47cce9cc6b7422091098
Mohammadreza Hami <mhdr.hami@gmail.com>
2024-11-23 17:43:22 -0700

    turned off the DH

--- papers/DSDWA/Driver.cpp
+++ papers/DSDWA/Driver.cpp
@@ -56,7 +56,7 @@ double Tcosts[4], rdm, hardness[4];
 bool showPlane = false;
 bool searchRunning = false;
 bool saveSVG = false;
-bool useDH = true;
+bool useDH = false;
 bool limitScenarios = false, normalizedSTP= false;
 int numLimitedScenarios = 1000, lowerLimit=50, upperLimit=2500, numScenario=598;
 bool flag = false;
